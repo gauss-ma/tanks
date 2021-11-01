ff9cd431799c336e8af3d5e8dca6e7fe295f6496
ramespada <espada@agro.uba.ar>
2021-11-01 14:21:57 -0300

    funcion para debug

---
+++ js/debug.js
@@ -0,0 +1,21 @@
+function debugRun(){
+
+
+
+	console.log("Iniciando corrida de debuggeo..");
+
+	// cargar variables:
+	//
+	//
+	
+	// llamar a función para hacer cálculos
+	//
+	//
+	
+	// output results:
+	//
+	//outputWindow()
+
+
+	console.log("Fin corrida de debuggeo.");
+}
